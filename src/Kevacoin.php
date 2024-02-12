0e33447c915a9a955510c89019ae645aa9397f9d
ghost <localhost>
2024-02-12 21:08:38 +0200

    add Kevacoin utils

---
+++ src/Kevacoin.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kvazar\Crypto;
+
+class Kevacoin
+{
+    function decode(string $value)
+    {
+        if (is_numeric($string) && $string < 0xFFFFFFFF)
+        {
+            return mb_chr(
+                $string,
+                'ASCII'
+            );
+        }
+
+        else
+        {
+            return hex2bin(
+                $string
+            );
+        }
+    }
+}
